0646aec229cc707dfabc3e55d607ac0b088ddfad
unknown <muotar@3se-spel-09.utb.fuv.se>
2016-03-02 14:34:11 +0100

    Satte in mitt projekt (floppy boy) i DoG

---
+++ Assets/Games/Arthur/Floppy Boy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpHeight;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+            //Time.timeScale = 1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            //Time.timeScale = 1;
+        }
+
+
+    }
+}
