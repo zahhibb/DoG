c13a3699ca7b60e41418079a6adf48726e75add8
plantpaladin <plantpaladin@gmail.com>
2016-03-08 16:42:42 +0100

    färdigt för testning
    Flyttade mina skript till min mapp och gjorde klart skripten, hoppas att
    det räcker

--- Assets/Games/Anton/PlayerScript.cs
+++ Assets/Games/Anton/PlayerScript.cs
@@ -4,18 +4,22 @@ using System.Collections;
 public class PlayerScript : MonoBehaviour {
     private Manager m_manager;
     private Rigidbody2D m_rb2d;
+    private float m_speed;
     public void AssignManager(Manager manager)
     {
         m_manager = manager;
     }
     // Use this for initialization
     void Start () {
+        m_speed = 1;
+        m_rb2d = GetComponent<Rigidbody2D>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Input.GetAxis(m_manager.Inputs[6].name);
-
+        float x = Input.GetAxis(m_manager.Inputs[6].name);
+        float y = Input.GetAxis(m_manager.Inputs[7].name);
+        m_rb2d.AddForce(new Vector2(x*m_speed,y*m_speed));
 
     }
 }
--- Assets/Games/Anton/collisionShot.cs
+++ Assets/Games/Anton/collisionShot.cs
@@ -13,21 +13,26 @@ public class collisionShot : MonoBehaviour {
 	void Update () {
         
         if (shallDestroy)
-        {
+        {//destroys the object if it has collided with a player since last frame
             Destroy(gameObject);
         }
         else if ((transform.position.x < -32)
             || (transform.position.x > Screen.width + 32)
             || (transform.position.y < -32)
             || (transform.position.y > Screen.height + 32))
-        {
+        {//or if it is outside of bounds
             Destroy(gameObject);
         }
 
     }
 
-    void OnCollisionTrigger()
-    {
+    void OnCollisionEnter(Collision collision)
+    {//destroys the shot if it collides with a player
         shallDestroy = true;
+        if (collision.gameObject.tag == "Player")
+        {//doing instant kill right now, keeps up the speed and makes the code simple
+            //used http://answers.unity3d.com/questions/124126/how-to-make-a-player-die-on-collision.html for inspiration
+            Destroy(collision.gameObject);
+        }
     }
 }
