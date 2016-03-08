79aefc9a54ed85b47b568798a19464651494b09a
HappyLittleGames <isak.sondell@gmail.com>
2016-03-08 02:59:35 +0100

    Tutorialscenen har ingenting som gör att börjar/slutar, men är i övrig brukbar

---
+++ Assets/Tutorial/Scripts/PlayImage.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayImage : MonoBehaviour
+{
+    [SerializeField] private bool m_startOnLoad = false;
+    private RawImage m_myImage;
+    private MovieTexture m_movie;  
+
+    void Awake()
+    {
+        m_myImage = gameObject.GetComponent<RawImage>();
+        m_movie = (MovieTexture)m_myImage.texture;
+        m_movie.loop = true;
+        m_movie.Stop();
+
+    }
+
+    void Start()
+    {
+        if (m_startOnLoad)
+        {
+            m_movie.Play();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (m_movie.isPlaying)
+            {
+                m_movie.Pause();
+            }
+            else
+            {
+                m_movie.Play();
+            }
+        }
+    }
+    
+    public void PlayLoop()
+    {
+        m_movie.Play();
+    }
+
+    public void PauseLoop()
+    {
+        RawImage r = gameObject.GetComponent<RawImage>();
+        MovieTexture movie = (MovieTexture)r.texture;
+        movie.loop = true;
+    }
+}
+
--- Assets/Tutorial/Scripts/SceneRouter.cs
+++ Assets/Tutorial/Scripts/SceneRouter.cs
@@ -1,22 +1,51 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class SceneRouter : MonoBehaviour {
 
-    private string m_sceneChoice;
-    private float m_totalTime = 2f;
+    [SerializeField] private string m_sceneChoice;
+    [SerializeField] private float m_totalTime = 2f;
+    [SerializeField] private Canvas m_countDownCanvas;
+    [SerializeField] GameObject m_spinCountdown;
+
+    private bool m_isCounting = false;
+    private float m_lastTime = 0f;
 
     // serialize your very own tutorial here.
-
     [SerializeField] Object m_sampleTutorial = null;
 
 	private void Start ()
     {
-        m_sceneChoice = GameObject.FindGameObjectWithTag("ManagerP1").GetComponent<Manager>().ChosenScene;
+        //m_sceneChoice = GameObject.FindGameObjectWithTag("ManagerP1").GetComponent<Manager>().ChosenScene;
         MakeTutorial(m_sceneChoice);
 
         // there should be some fancy count down animated too
         StartCoroutine(CountDown(m_totalTime));
+        SpinCountdown();
+    }
+
+    private void Update()
+    {
+        if (m_isCounting)
+        {
+            if (Time.timeSinceLevelLoad - m_lastTime >= 3)
+            {
+                m_countDownCanvas.GetComponentInChildren<Text>().text = "go.";
+            }
+            else if (Time.timeSinceLevelLoad - m_lastTime >= 2)
+            {
+                m_countDownCanvas.GetComponentInChildren<Text>().text = "1";
+            }
+            else if (Time.timeSinceLevelLoad - m_lastTime >= 1)
+            {
+                m_countDownCanvas.GetComponentInChildren<Text>().text = "2";
+            }
+        }
+        else
+        {
+            m_lastTime = Time.timeSinceLevelLoad;
+        }
     }
 
     private void MakeTutorial(string choice)
@@ -45,4 +74,13 @@ public class SceneRouter : MonoBehaviour {
         yield return new WaitForSeconds(time);
         UnityEngine.SceneManagement.SceneManager.LoadScene(m_sceneChoice);
     }
+
+    private void SpinCountdown()
+    {
+        m_countDownCanvas.GetComponentInChildren<Text>().text = "3";
+        m_countDownCanvas.GetComponentInChildren<PlayImage>().PlayLoop();
+        m_isCounting = true;
+    }
+
+
 }
