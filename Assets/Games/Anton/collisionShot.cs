24f66a2c2dd3d25c771abb04c00266aa54682cdd
plantpaladin <johaan@utb.fuv.se>
2016-03-09 15:59:23 +0100

    Working
    The level is working as intended

--- Assets/Games/Anton/collisionShot.cs
+++ Assets/Games/Anton/collisionShot.cs
@@ -2,37 +2,46 @@
 using System.Collections;
 
 public class collisionShot : MonoBehaviour {
-    bool shallDestroy;//not instant destruction
+    bool m_shallDestroy;//not instant destruction
 	// Use this for initialization
 	void Start () {
-        shallDestroy = false;
-        
-	}
+        m_shallDestroy = false;
+        Vector3 temp = transform.position;
+        temp.z = 0.0f;
+        transform.position = temp;//ensure all parts have same z value http://answers.unity3d.com/questions/600421/how-to-change-xyz-values-in-a-vector3-properly-in.html
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
         
-        if (shallDestroy)
+        if (m_shallDestroy)
         {//destroys the object if it has collided with a player since last frame
             Destroy(gameObject);
         }
-        else if ((transform.position.x < -32)
+        /*else if ((transform.position.x < -32)
             || (transform.position.x > Screen.width + 32)
             || (transform.position.y < -32)
             || (transform.position.y > Screen.height + 32))
         {//or if it is outside of bounds
             Destroy(gameObject);
-        }
+        }*/
 
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {//destroys the shot if it collides with a player
-        shallDestroy = true;
         if (collision.gameObject.tag == "Player")
-        {//doing instant kill right now, keeps up the speed and makes the code simple
-            //used http://answers.unity3d.com/questions/124126/how-to-make-a-player-die-on-collision.html for inspiration
-            Destroy(collision.gameObject);
+        {
+            m_shallDestroy = true;
+            print("blue");
+            collision.gameObject.GetComponent<PlayerScript>().SetShallDestroy();
+
+        }
+        else if (collision.gameObject.tag =="Wall")
+        {
+            m_shallDestroy = true;
+
         }
     }
 }
