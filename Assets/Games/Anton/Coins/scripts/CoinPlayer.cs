13fbe31680fc4f52a9aa602532ba497c9f993b34
plantpaladin <johaan@utb.fuv.se>
2016-03-18 14:56:09 +0100

    reorganized folders and begun new game

---
+++ Assets/Games/Anton/Coins/scripts/CoinPlayer.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinPlayer : MonoBehaviour {
+    private Manager m_manager;
+    private Rigidbody2D m_rb2d;
+    private float m_speed;
+    private bool m_shallDestroy;
+    public void AssignManager(Manager manager)
+    {
+        m_manager = manager;
+        GetComponent<SpriteRenderer>().color = m_manager.PlayerColor;
+    }
+    // Use this for initialization
+    void Start() {
+        m_speed = 5;
+        m_rb2d = GetComponent<Rigidbody2D>();
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Shot")
+        {
+            print("blue");
+            m_shallDestroy = true;
+        }
+    }
+    public void SetPlayerPosition(Vector3 NewPosition)
+    {
+        transform.position = NewPosition;
+    }
+    public bool GetShallDestroy()
+    {//returns if the player has hit a shot
+        return m_shallDestroy;
+    }
+    public void SetShallDestroy()
+    {//called by the shot the player hits
+        m_shallDestroy = true;
+    }
+    public void SetScore(int score)
+    {//sets the score of the player(used just before it is destroyed)
+        m_manager.Score += score;
+    }
+// Update is called once per frame
+void Update() {
+        float y = -Input.GetAxis(m_manager.Inputs[13].name);
+
+        float x = Input.GetAxis(m_manager.Inputs[12].name);
+
+        m_rb2d.AddForce(new Vector2(x * m_speed, y * m_speed));
+
+    }
+    
+}
---
+++ Assets/Games/Anton/Coins/scripts/CoinPlayerSpawner.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+public class CoinPlayerSpawner : MonoBehaviour {
+    [SerializeField]
+    GameObject m_player;//is set inside the Unity Engine
+                        // Use this for initialization
+    private List<GameObject> m_players;
+    private double m_width;//half the width
+    private double m_height;//half the height
+    void Start ()
+    {
+        m_width = Camera.main.orthographicSize;
+        m_height = Camera.main.orthographicSize * (Screen.width / Screen.height);
+        m_players = new List<GameObject>();
+        int loop = GameObject.FindGameObjectWithTag("ManagerP1").GetComponent<Manager>().Controllers;
+        for (int i = 0; i < loop; i++)
+        {
+            
+            m_players.Add(Instantiate(m_player));
+            m_players[i].GetComponent<PlayerScript>().AssignManager(GameObject.FindGameObjectWithTag("ManagerP"+(i+1)).GetComponent<Manager>());
+            switch (i)
+            {
+                case 0:
+                    m_players[i].GetComponent<PlayerScript>().SetPlayerPosition(new Vector3((float)(m_width*-1.6),(float)(m_height*0.4),0));
+                    break;
+                case 1:
+                    m_players[i].GetComponent<PlayerScript>().SetPlayerPosition(new Vector3((float)(m_width * 1.6), (float)(m_height * 0.4), 0));
+                    break;
+                case 2:
+                    m_players[i].GetComponent<PlayerScript>().SetPlayerPosition(new Vector3((float)(m_width * -1.6), (float)(m_height * -0.4), 0));
+                    break;
+                case 3:
+                    m_players[i].GetComponent<PlayerScript>().SetPlayerPosition(new Vector3((float)(m_width * 1.6), (float)(m_height * -0.4), 0));
+                    break;
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i=0;i< m_players.Count;i++)
+        {
+            if (m_players[i].GetComponent<PlayerScript>().GetShallDestroy()==true)
+            {
+
+                m_players[i].GetComponent<PlayerScript>().SetScore(8-m_players.Count * 2);
+                Destroy(m_players[i]);
+                m_players.RemoveAt(i);
+            }
+        }
+        if (m_players.Count == 0)
+        {
+            SceneManager.LoadScene("Staging");
+        }
+    }
+}
