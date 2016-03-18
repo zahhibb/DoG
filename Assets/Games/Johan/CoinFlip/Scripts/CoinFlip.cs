3c7632eee04fc59551564cf7f2d51bb7dd6aa59a
unknown <eriksjo@3se-spel-23.utb.fuv.se>
2016-03-18 16:11:26 +0100

    Coinflip + 3d coins

--- Assets/Games/Johan/CoinFlip/Scripts/CoinFlip.cs
+++ Assets/Games/Johan/CoinFlip/Scripts/CoinFlip.cs
@@ -19,22 +19,27 @@ public class CoinFlip : MonoBehaviour {
     [SerializeField]
     private int m_previousNumber2 = -1;
 
+    private bool m_canFlip = true;
+
     // Use this for initialization
     void Start () 
     {
 
-        //m_playerManagers = new List<Manager>();
-        //Manager player1Manager = GameObject.FindGameObjectWithTag("ManagerP1").GetComponent<Manager>();
-        //m_playerAmount = player1Manager.Controllers;
+        m_playerManagers = new List<Manager>();
+        Manager player1Manager = GameObject.FindGameObjectWithTag("ManagerP1").GetComponent<Manager>();
+        m_playerAmount = player1Manager.Controllers;
 
-        //// glöm ej att ta bort denna skit
-        //m_playerManagers.Add(player1Manager);
-        //m_playerManagers.Add(player1Manager);
-        //m_playerManagers.Add(player1Manager);
-        //m_playerManagers.Add(player1Manager);
+        // glöm ej att ta bort denna skit
+        m_playerManagers.Add(player1Manager);
+        m_playerManagers.Add(player1Manager);
+        m_playerManagers.Add(player1Manager);
+        m_playerManagers.Add(player1Manager);
 
-
-        FlipCoin();
+       
+        
+            
+            FlipCoin();
+        
     }
 	
 	// Update is called once per frame
@@ -46,90 +51,120 @@ public class CoinFlip : MonoBehaviour {
 
     void FlipCoin()
     {
-        if(m_rolls == 3)
+        if (m_canFlip == true)
         {
-            GoToCelebration();
-        }
+            while (m_coinValue == m_previousNumber || m_coinValue == m_previousNumber1 || m_coinValue == m_previousNumber2)
+            {
+                m_coinValue = Random.Range(0, 4);
+            }
 
-        m_coinValue = Random.Range(0, 3);
+            if (m_rolls == 0)
+            {
+                m_previousNumber = m_coinValue;
+            }
 
-        while (m_coinValue == m_previousNumber || m_coinValue == m_previousNumber1 || m_coinValue == m_previousNumber2)
-        {
-            m_coinValue = Random.Range(0, 3);
-        }
-        if (m_rolls == 0)
-        {
-            m_previousNumber = m_coinValue;
-        }
+            else if (m_rolls == 1)
+            {
+                m_previousNumber1 = m_coinValue;
+            }
 
-        else if (m_rolls == 1)
-        {
-            m_previousNumber1 = m_coinValue;
-        }
+            else if (m_rolls == 2)
+            {
+                m_previousNumber2 = m_coinValue;
+            }
+           
 
-        else if (m_rolls == 2)
-        {
-            m_previousNumber2 = m_coinValue;
-        }
+            switch (m_coinValue)
+            {
+                case 0:
+                    m_rolls++;
+                    Debug.Log("0");
+                    GreenGranolaWin();
 
-    switch (m_coinValue)
-        {
-            case 0:
-              GreenGranolaWin();
-                
-                break;
+                    break;
 
-            case 1:
-                PinkPelicanWin();
-                
-                break;
+                case 1:
+                    m_rolls++;
+                    Debug.Log("1");
+                    PinkPelicanWin();
 
-            case 2:
-               LemonTruckWin();
-                
-                break;
+                    break;
 
-            case 3:
-                BlueCoatHangerWin();
-                
-                break;
+                case 2:
+                    Debug.Log("2");
+                    m_rolls++;
+                    LemonTruckWin();
+
+                    break;
+
+                case 3:
+                    Debug.Log("3");
+                    m_rolls++;
+                    BlueCoatHangerWin();
+
+                    break;
+            }
         }
     }
 
     void GreenGranolaWin()
     {
-      //  m_playerManagers[0].Score += m_currentScore;
-        m_rolls += 1;
+        m_playerManagers[0].Score += m_currentScore;
+        
         m_currentScore /= 2;
+        if (m_rolls == 4)
+        {
+            m_canFlip = false;
+            GoToCelebration();
+        }
+
         FlipCoin();
     }
 
     void PinkPelicanWin()
     {
-     //   m_playerManagers[1].Score += m_currentScore;
-        m_rolls += 1;
+        m_playerManagers[1].Score += m_currentScore;
+        
         m_currentScore /= 2;
+        if (m_rolls == 4)
+        {
+            m_canFlip = false;
+            GoToCelebration();
+        }
+
         FlipCoin();
     }
 
     void LemonTruckWin()
     {
-      //  m_playerManagers[2].Score += m_currentScore;
-        m_rolls += 1;
+        m_playerManagers[2].Score += m_currentScore;
+        
         m_currentScore /= 2;
+        if (m_rolls == 4)
+        {
+            m_canFlip = false;
+            GoToCelebration();
+        }
+
         FlipCoin();
     }
 
     void BlueCoatHangerWin()
     {
-    //    m_playerManagers[3].Score += m_currentScore;
-        m_rolls += 1;
+        m_playerManagers[3].Score += m_currentScore;
+        
         m_currentScore /= 2;
+        if (m_rolls == 4)
+        {
+            m_canFlip = false;
+            GoToCelebration();
+        }
+
         FlipCoin();
     }
 
     void GoToCelebration()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Celebration");
+        //UnityEngine.SceneManagement.SceneManager.LoadScene("Celebration");
     }
 }
